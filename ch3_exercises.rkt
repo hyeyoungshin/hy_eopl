7d324b284090a845e2783a6e8abe7b0950193dc3
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2018-06-17 18:19:49 -0400

    error: bad value for rands field

--- ch3_exercises.rkt
+++ ch3_exercises.rkt
@@ -41,32 +41,55 @@
       [(number? x) (a-program (lit-exp x))]
       [(symbol? x) (a-program (var-exp x))]
       [(list? x) (a-program
-                  (primapp-exp
                    (let ([fst (car x)]
-                         [rest (parse-args (cdr x))])
+                         [rands (parse-args (cdr x))])
                      (cond
-                       [(eqv? fst '+) add-prim rest]
-                       [(eqv? fst '-) sub-prim rest]
-                       [(eqv? fst '*) mul-prim rest]
-                       [(eqv? fst 'add1) add1-prim rest]
-                       [(eqv? fst 'sub1) sub1-prim rest]))))]
+                       [(eqv? fst '+) (primapp-exp (add-prim) rands)]
+                       [(eqv? fst '-) (primapp-exp (sub-prim) rands)]
+                       [(eqv? fst '*) (primapp-exp (mul-prim) rands)]
+                       [(eqv? fst 'add1) (primapp-exp (add1-prim) rands)]
+                       [(eqv? fst 'sub1) (primapp-exp (sub1-prim) rands)]
+                       [else eopl:error])))]
       [else eopl:error])))
 
+; SL -> List-of expression (or error)
 (define parse-args
   (lambda (xs)
-    (let ([fst (car xs)]
-          [rest (parse-args (cdr xs))])
+    (cond
+      [(null? xs) '()]
+      [(eqv? (length xs) 1) (parse-arg (car xs))]
+      [else ; list of at least length 2
+       (cond
+         [(list? (car xs)) (append (primapp-exp (parse-args (car xs)) (parse-args (cdr xs))))]
+         [else (cons (parse-arg (car xs)) (parse-args (cdr xs)))])])))
+
+
+
+
+; Atom -> List-of expression (or error)
+(define parse-arg
+  (lambda (x)
+    (cond
+      [(number? x) '(lit-exp x)]
+      [(symbol? x) (cond
+                     [(eqv? x '+) '(add-prim)]
+                     [(eqv? x '-) '(sub-prim)]
+                     [(eqv? x '*) '(mul-prim)]
+                     [(eqv? x 'add1) '(add1-prim)]
+                     [(eqv? x 'sub1) '(sub1-prim)]
+                     [else eopl:error])]
+      [else eopl:error])))
+
+
+(define arity-check
+  (lambda (rst n)
+    (let ([l (length rst)]) 
       (cond
-        [(number? fst) ((lit-exp fst) rest)]
-        [(symbol? fst) (cond
-                              [(eqv? fst '+) add-prim rest]
-                              [(eqv? fst '-) sub-prim rest]
-                              [(eqv? fst '*) mul-prim rest]
-                              [(eqv? fst 'add1) add1-prim rest]
-                              [(eqv? fst 'sub1) sub1-prim rest]
-                              [else (var-exp fst) rest])]
+        [(eqv? n l) rst]
         [else eopl:error]))))
-                            
+      
+
+
 
     
 
