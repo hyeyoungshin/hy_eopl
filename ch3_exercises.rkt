02d5657c3c8954ca3562e816fee76818325e6f56
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2018-06-19 19:21:02 -0400

    works

--- ch3_exercises.rkt
+++ ch3_exercises.rkt
@@ -1,5 +1,7 @@
 #lang eopl
 
+(require rackunit)
+
 ; An S-expr is one of: 
 ; – Atom
 ; – SL
@@ -39,7 +41,7 @@
   (lambda (x)
     (a-program (sexp-to-exp x))))
 
-; Sexp -> Expression
+; Sexp -> Expression (or error)
 (define sexp-to-exp
   (lambda (x)
     (cond
@@ -57,7 +59,7 @@
       [(eqv? x '*) (mul-prim)]
       [(eqv? x 'add1) (add1-prim)]
       [(eqv? x 'sub1) (sub1-prim)]
-      [else eopl:error])))
+      [else (eopl:error 'sexp-to-prim"~s not a primitive operation" x)])))
 
 
 ; List-of Sexpr -> List-of Expression
@@ -66,7 +68,31 @@
     (cond
       [(null? a) '()]
       [else (cons (sexp-to-exp (car a)) (sexp-to-list (cdr a)))])))
+
+(check-equal? (sexp-to-program '(add1 (+ 3 x)))
+                               (a-program
+                                (primapp-exp
+                                 (add1-prim)
+                                 (list (primapp-exp
+                                        (add-prim)
+                                        (list (lit-exp 3)
+                                              (var-exp 'x)))))))
     
+;'(add1 (+ 3 x)) =>
+; (a-program
+;   (prim-app
+;     (add1-prim)
+;     (list (prim-app
+;             (add-prim)
+;             (list (lit-exp 3)
+;                   (var-exp x)))
+
+;'(add1 (add1 4))
+;'(* (add1 4) (add1 5))
+; 2
+; 'x
+; '(+ (- 3 x) (* (+ 2 x) (add1 5))
+
 
 
     
