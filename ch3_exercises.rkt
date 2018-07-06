f4c754f38fa951cebae3f328e6100a76a661c8b6
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2018-07-06 19:15:52 -0400

    closed? function needs fix

--- ch3_exercises.rkt
+++ ch3_exercises.rkt
@@ -1,6 +1,7 @@
 #lang plai-typed
 
 (require (typed-in racket [ormap : (('T -> boolean) (listof 'T) -> boolean)]))
+(require (typed-in racket [andmap : (('T -> boolean) (listof 'T) -> boolean)]))
 ;(require (only-in lang/htdp-beginner ...))
 (require plai-typed/s-exp-match)
 
@@ -42,20 +43,20 @@
   (primapp-exp
    (prim : primitive)
    (rands : (listof expression)))
-  (if-exp
+  #;(if-exp
    (test-exp : expression)
    (true-exp : expression)
    (false-exp : expression))
   (let-exp
-   (ids : (listof symbol))
-   (rands : (listof expression))
+   (id : symbol)
+   (rand : expression)
    (body : expression))
   (proc-exp
-   (ids : (listof symbol))
+   (id : symbol)
    (body : expression))
   (app-exp
    (rator : expression)
-   (rands : (listof expression))))
+   (rand : expression)))
 
 (define-type primitive
   (add-prim) ; add-prim is a function if you do (add-prim) you are calling it
@@ -63,6 +64,12 @@
   (mul-prim)
   (add1-prim)
   (sub1-prim))
+
+(define-type value
+  (num (n : number))
+  (fun (f : proc)))
+  
+  
 
 ; For environment passing interpreter
 #;(define-type procval procval?
@@ -72,9 +79,9 @@
     (env environment?)))
 
 ; For substitution based interpreter
-#;(define-type proc proc?
-  (lam (ids (list-of symbol?))
-       (body expression?)))
+(define-type proc 
+  (lam (id : symbol)
+       (body : expression)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; The Front End (Sexpr -> AST)
@@ -122,14 +129,7 @@
                                         (list (lit-exp 3)
                                               (var-exp 'x)))))))
     
-(define apply-primitive
-  (lambda (prim args)
-    (type-case primitive prim
-      (add-prim () (+ (first args) (second args)))
-      (sub-prim () (- (first args) (second args)))
-      (mul-prim () (* (first args) (second args)))
-      (add1-prim () (+ (first args) 1))
-      (sub1-prim () (- (first args) 1)))))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Examples:
@@ -156,82 +156,139 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Substitution based interpreter
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; expression -> bool
+(define closed? : (expression -> boolean)
+  (lambda (e)
+    (type-case expression e
+      (lit-exp (datum) true)
+      (var-exp (id) false)
+      (primapp-exp (prim rands) (andmap (lambda (r) (closed? r)) rands))
+      (let-exp (id rand body) (closed? (subst body id rand)))
+      (proc-exp (id body) (type-case expression body
+                            (var-exp (x) (symbol=? id x))
+                            (else (closed? body))))
+      (app-exp (rator rand) (and (closed? rator) (closed? rand))))))
+                
+      
+
+; expression symbol expression -> expression
 (define subst
-  (lambda (exp sub-id val)
-    (let ((subst-rands (lambda (rands) (map (lambda (rand) (subst rand sub-id val)) rands)))
+  (lambda (exp sub-id val-exp)
+    (let ((subst-rands (lambda (rands) (map (lambda (rand) (subst rand sub-id val-exp)) rands)))
           (redundant? (lambda (ids) (ormap (lambda (id) (symbol=? sub-id id)) ids))))
-      (type-case expression exp
-        (lit-exp (datum) exp)
-        (var-exp (id) (if (equal? id sub-id)
-                          val
-                          exp))
-        (primapp-exp (prim rands)
-                     (primapp-exp prim (subst-rands rands)))
-        (if-exp (test-exp true-exp false-exp)
-                (if-exp (subst test-exp sub-id val)
-                        (subst true-exp sub-id val)
-                        (subst false-exp sub-id val)))
-        (let-exp (ids rands body)
-                 (if (redundant? ids)
-                     exp
-                     (let-exp ids (subst-rands rands) (subst body sub-id val))))
-        (proc-exp (ids body)
-                  (if (redundant? ids)
-                      exp
-                      (proc-exp ids (subst body sub-id val))))
-        (app-exp (rator rands)
-                 (app-exp (subst rator sub-id val) (subst-rands rands)))))))
+      (if (closed? val-exp)
+          (type-case expression exp
+            (lit-exp (datum) exp)
+            (var-exp (id) (if (equal? id sub-id)
+                              val-exp
+                              exp))
+            (primapp-exp (prim rands)
+                         (primapp-exp prim (subst-rands rands)))
+            #;(if-exp (test-exp true-exp false-exp)
+                      (if-exp (subst test-exp sub-id val-exp)
+                              (subst true-exp sub-id val-exp)
+                              (subst false-exp sub-id val-exp)))
+            (let-exp (id rand body)
+                     (if (symbol=? id sub-id)
+                         exp
+                         (let-exp id (subst rand sub-id val-exp) (subst body sub-id val-exp))))
+            (proc-exp (id body)
+                      (if (symbol=? id sub-id)
+                          exp
+                          (proc-exp id (subst body sub-id val-exp))))
+            (app-exp (rator rand)
+                     (app-exp (subst rator sub-id val-exp) (subst rand sub-id val-exp))))
+          (error 'subst "can't substitute an open term")))))
 
 (test (subst (lit-exp 5) 'x (lit-exp 3)) (lit-exp 5))
 (test (subst (var-exp 'x) 'x (lit-exp 3)) (lit-exp 3))
 (test (subst (var-exp 'x) 'y (lit-exp 3)) (var-exp 'x))
-(test (subst (proc-exp (list 'x) (primapp-exp (add-prim) (list (var-exp 'x) (var-exp 'y)))) 'y (lit-exp 3))
-              (proc-exp (list 'x) (primapp-exp (add-prim) (list (var-exp 'x) (lit-exp 3)))))
-(test (subst (proc-exp (list 'x) (primapp-exp (add-prim) (list (var-exp 'x) (lit-exp 3)))) 'x (lit-exp 5))
-              (proc-exp (list 'x) (primapp-exp (add-prim) (list (var-exp 'x) (lit-exp 3)))))
-(test (subst (proc-exp (list 'f 'x) (app-exp (var-exp 'f) (list (var-exp 'x)))) 'f (proc-exp (list 'x) (var-exp 'x)))
-      (proc-exp (list 'f 'x) (app-exp (var-exp 'f) (list (var-exp 'x)))))
-(test (subst (proc-exp (list 'x) (var-exp 'y)) 'y (var-exp 'x))
-      (proc-exp (list 'z) (var-exp 'x))) ;<-------------------------------------------------------------------------------------------------------------failing
-(test (subst (proc-exp (list 'f 'x) (app-exp (var-exp 'g) (list (var-exp 'x)))) 'g (proc-exp (list 'x) (var-exp 'x)))
-      (proc-exp (list 'f 'x) (app-exp (proc-exp (list 'x) (var-exp 'x)) (list (var-exp 'x)))))
+(test (subst (proc-exp 'x (primapp-exp (add-prim) (list (var-exp 'x) (var-exp 'y)))) 'y (lit-exp 3))
+              (proc-exp 'x (primapp-exp (add-prim) (list (var-exp 'x) (lit-exp 3)))))
+(test (subst (proc-exp 'x (primapp-exp (add-prim) (list (var-exp 'x) (lit-exp 3)))) 'x (lit-exp 5))
+              (proc-exp 'x (primapp-exp (add-prim) (list (var-exp 'x) (lit-exp 3)))))
+(test (subst (proc-exp 'f (proc-exp 'x (app-exp (var-exp 'f) (var-exp 'x)))) 'f (proc-exp 'x (var-exp 'x)))
+      (proc-exp 'f (proc-exp 'x (app-exp (var-exp 'f) (var-exp 'x)))))
+(test/exn (subst (proc-exp 'x (var-exp 'y)) 'y (var-exp 'x))
+      "can't substitute an open term") ;<-------------------------------------------------------------------------------------------------------------failing
+
+; Solution: 
+; I make the interpreter not allow a free variable to be applied to a function.
+; A free variable should not appear in function's argument position.
+(test (subst (proc-exp 'f (proc-exp 'x (app-exp (var-exp 'g) (var-exp 'x)))) 'g (proc-exp 'x (var-exp 'x)))
+      (proc-exp 'f (proc-exp 'x (app-exp (proc-exp 'x (var-exp 'x)) (var-exp 'x)))))
 
     
+; listof expression -> value
+(define eval-rands-subst
+  (lambda ([rands : (listof expression)])
+    (map (lambda (r) (eval-rand-subst r)) rands)))
 
-#;(define eval-rands-subst
-  ...)
+; expression -> value
+(define eval-rand-subst
+  (lambda ([rand : expression])
+    (eval-expression-subst rand)))
+
+; primitive listof expression -> value
+(define apply-primitive : (primitive (listof value) -> value)
+  (lambda (prim args)
+    (type-case primitive prim
+      (add-prim () (num (+ (val-to-num (first args)) (val-to-num (second args)))))
+      (sub-prim () (num (- (val-to-num (first args)) (val-to-num (second args)))))
+      (mul-prim () (num (* (val-to-num (first args)) (val-to-num (second args)))))
+      (add1-prim () (num (+ (val-to-num (first args)) 1)))
+      (sub1-prim () (num (- (val-to-num (first args)) 1))))))
                    
-
-#;(define eval-program-subst
-  (lambda (p)
+; program -> value
+(define eval-program-subst
+  (lambda ([p : program])
     (type-case program p
       (a-program (body)
-                 (eval-expression body)))))
+                 (eval-expression-subst body)))))
 
-#;(define eval-expression-subst
-    (lambda (exp)
-      (type-case expression exp
-        (lit-exp (datum) datum)
-        (var-exp (id) (eopl:error "free identifier ~s" id))
-        (primapp-exp (prim rands)
-                     (let ((args (eval-rands rands env)))
-                       (apply-primitive prim args)))
-        (if-exp (test-exp true-exp false-exp)
-                (if (true-value? (eval-expression test-exp))
-                    (eval-expression true-exp)
-                    (eval-expression false-exp)))
-        (let-exp (ids rands body)
-                 (let ((args (eval-rands rands env)))
-                   (eval-expression body (extend-env ids args env))))
-        (proc-exp (ids body) (closure ids body env))
-        (app-exp (rator rands)
-                 (let ((proc (eval-expression rator env))
-                       (args (eval-rands rands env)))
-                   (if (procval? proc)
-                       (apply-procval proc args)
-                       (eopl:error "Attempt to apply non-procedure ~s" proc)))))))
+(define val-to-exp
+  (lambda ([v : value])
+    (type-case value v
+      (num (n) (lit-exp n))
+      (fun (f) (type-case proc f
+                 (lam (id body) (proc-exp id body)))))))
 
+(define val-to-num
+  (lambda (v)
+    (type-case value v
+      (num (n) n)
+      (fun (f) (error 'val-to-num "not a number")))))
+  
+; expression -> value or error
+(define eval-expression-subst : (expression -> value)
+  (lambda ([exp : expression])
+    (type-case expression exp
+      (lit-exp (datum) (num datum))
+      (var-exp (id) (error 'eval-expression-subst (string-append (to-string id) "is a free identifier")))
+      (primapp-exp (prim rands)
+                   (let ((args (eval-rands-subst rands)))
+                     (apply-primitive prim args)))
+      #;(if-exp (test-exp true-exp false-exp)
+                (if (true-value? (eval-expression-subst test-exp))
+                    (eval-expression-subst true-exp)
+                    (eval-expression-subst false-exp)))
+      (let-exp (id rand body)
+               (let ((arg (val-to-exp (eval-expression-subst rand)))); call-by-value
+                 (eval-expression-subst (subst body id arg))))
+      (proc-exp (id body) (fun (lam id body)))
+      (app-exp (rator rand)
+               (let ((function (eval-expression-subst rator))
+                     (arg (val-to-exp (eval-expression-subst rand))))
+                 (type-case value function
+                   (fun (f)
+                        (type-case proc f
+                          (lam (id body)
+                               (eval-expression-subst (subst body id arg)))))
+                   (num (n)
+                        (error 'eval-expression-subst "Attempt to apply non-procedure"))))))))
 
+(test 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Environment passing interpreter
