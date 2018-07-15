effd877072e84398bc5e5fdf1d280e16601a3556
Hyeyoung Shin <hyeyoungshinw@gmail.com>
2018-07-15 13:54:46 -0700

    two representations for closure

--- env-interpreter.rkt
+++ env-interpreter.rkt
@@ -3,7 +3,7 @@
 (require (typed-in racket [ormap : (('T -> boolean) (listof 'T) -> boolean)]))
 (require (typed-in racket [andmap : (('T -> boolean) (listof 'T) -> boolean)]))
 ;(require (only-in lang/htdp-beginner ...))
-(require plai-typed/s-exp-match)
+;(require plai-typed/s-exp-match)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -27,10 +27,45 @@
   (lambda (x)
     (not (zero? x))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                              Closure                                  ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; In order for a procedure to retain the bindings that its free variables
+;; had at the time it was created, it must be a closed package, independent
+;; of the environment in which it is used.
+;; Such a package is called "closure"
+;; In order to be self-contained, a closure must contain the procedure body,
+;; the list of formal parameters, and the bindings of its free variables.
+
+;; Representation 1: Procedural (functional) representation
+;;   Define closure to have a value that is a procedure that expects an
+;;   argument list
+
 #;(define closure
   (lambda (ids body env)
     (lambda (args)
       (eval-expression body (extend-env ids args env)))))
+
+#;(define apply-procval
+    (lambda (proc args)
+      (proc args)))
+
+
+;; Representation 2: Abstract syntax tree (structural) representation
+    
+(define-type procval
+   (closure ;<------------------ tells how to build a procedure value
+    (ids : (list-of symbol))
+    (body : expression)
+    (env : environment)))
+
+(define apply-procval ; <------- tells how to apply a procedure value
+  (lambda (proc args)
+    (type-case procval proc
+      (closure (ids body env)
+        (eval-expression body (extend-env ids args env))))))
+  
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define-type program
   (a-program (exp : expression)))
@@ -52,11 +87,11 @@
    (rand : expression)
    (body : expression))
   (proc-exp
-   (id : symbol)
+   (ids : (listof symbol))
    (body : expression))
   (app-exp
    (rator : expression)
-   (rand : expression)))
+   (rands : (listof expression))))
 
 (define-type primitive
   (add-prim) ; add-prim is a function if you do (add-prim) you are calling it
@@ -67,18 +102,16 @@
 
 (define-type value
   (num (n : number))
-  (fun (f : proc)))
+  (fun (f : procval)))
   
-  
-
-; For environment passing interpreter
-#;(define-type procval procval?
-   (closure
-    (ids (list-of symbol?))
-    (body expression?)
-    (env environment?)))
-
-
+(define apply-primitive : (primitive (listof value) -> value)
+  (lambda (prim args)
+    (type-case primitive prim
+      (add-prim () (num (+ (val-to-num (first args)) (val-to-num (second args)))))
+      (sub-prim () (num (- (val-to-num (first args)) (val-to-num (second args)))))
+      (mul-prim () (num (* (val-to-num (first args)) (val-to-num (second args)))))
+      (add1-prim () (num (+ (val-to-num (first args)) 1)))
+      (sub1-prim () (num (- (val-to-num (first args)) 1))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; The Front End (Sexpr -> AST)                      ;;
@@ -191,6 +224,10 @@
   (lambda (rand env)
     (eval-expression rand env)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                           Environment                                 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Representation 1 : Ribs
 (define init-env
   (lambda ()
     (extend-env
